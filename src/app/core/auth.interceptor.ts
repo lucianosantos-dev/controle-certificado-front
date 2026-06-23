30189fc1b2665f232a280bdf49009613c7160683
Luciano Santos de Jesus Reis <lucianodev.contato@gmail.com>
2026-06-23 17:24:18 -0300

    fix: protege interceptor de rotas publicas

--- src/app/core/auth.interceptor.ts
+++ src/app/core/auth.interceptor.ts
@@ -1,6 +1,12 @@
 import { HttpInterceptorFn } from "@angular/common/http";
 
 export const authIntecerptor: HttpInterceptorFn = (req, next) => {
+    const rotasPublicas = ['/auth/login', '/usuarios'];
+
+    if (rotasPublicas.some(rota => req.url.includes(rota))) {
+        return next(req);
+    }
+
     const token = localStorage.getItem('token');
 
     if (token) {
@@ -11,5 +17,6 @@ export const authIntecerptor: HttpInterceptorFn = (req, next) => {
         });
         return next(requestClonada);
     }
+
     return next(req);
 }
